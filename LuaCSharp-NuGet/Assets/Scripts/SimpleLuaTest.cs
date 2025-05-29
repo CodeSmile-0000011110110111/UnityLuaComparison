18b5207e2183730841305b4dda13a5a7dba28fdd
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-05-29 16:35:21 +0200

    add LuaCSharp project

---
+++ LuaCSharp-NuGet/Assets/Scripts/SimpleLuaTest.cs
@@ -0,0 +1,37 @@
+using Lua;
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleLuaTest : MonoBehaviour
+{
+	private const String LuaFunctionName = "TestLuaFunction";
+	[SerializeField] private Int32 m_LuaFunctionCallIterations = 5000;
+
+	private LuaState m_LuaState;
+
+	private void Start()
+	{
+		var script = $"function {LuaFunctionName}()" +
+		             "end";
+
+		m_LuaState = LuaState.Create();
+		m_LuaState.DoStringAsync(script);
+	}
+
+	private void Update()
+	{
+		for (var i = 0; i < m_LuaFunctionCallIterations; i++)
+		{
+			var func = m_LuaState.Environment[LuaFunctionName];
+			var luaFunction = func.Read<LuaFunction>();
+			luaFunction.InvokeAsync(m_LuaState, null);
+		}
+	}
+
+	public void SetIterations(Slider slider)
+	{
+		m_LuaFunctionCallIterations = (int)slider.value;
+	}
+}
