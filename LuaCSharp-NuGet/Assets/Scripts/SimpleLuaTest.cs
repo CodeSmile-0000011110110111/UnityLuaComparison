d60c3594d2c7294fe7e7dbd1fd42b9a054d53a63
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-04 16:21:46 +0200

    expand tests

--- LuaCSharp-NuGet/Assets/Scripts/SimpleLuaTest.cs
+++ LuaCSharp-NuGet/Assets/Scripts/SimpleLuaTest.cs
@@ -1,38 +1,83 @@
 using Lua;
 using Lua.Runtime;
 using System;
+using System.Threading.Tasks;
+using Unity.Profiling;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SimpleLuaTest : MonoBehaviour
 {
-	private const String LuaFunctionName = "TestLuaFunction";
+	private const String CallLuaFunction = "CallLuaFunction";
+	private const String CallCSharpMethod = "CallCSharpMethod";
 	[SerializeField] private Int32 m_LuaFunctionCallIterations = 5000;
 
 	private LuaState m_LuaState;
 
+	private ProfilerMarker m_LookupMarker;
+	private ProfilerMarker m_CallLuaMarker;
+	private ProfilerMarker m_CallCSharpMarker;
+
+
 	private void Start()
 	{
-		var script = $"function {LuaFunctionName}()" +
-		             "end";
+		var script = $"function {CallLuaFunction}()\n" +
+		             "end\n" +
+		             $"function {CallCSharpMethod}()\n" +
+		             "UsedByLua.CSharpMethod()\n" +
+		             "end\n";
 
 		m_LuaState = LuaState.Create();
 		m_LuaState.DoStringAsync(script);
+		m_LuaState.Environment["UsedByLua"] = new LuaValue(new UsedByLua());
+
+		m_LookupMarker = new ProfilerMarker("FunctionLookup");
+		m_CallLuaMarker = new ProfilerMarker(CallLuaFunction);
+		m_CallCSharpMarker = new ProfilerMarker(CallCSharpMethod);
 	}
 
 	private void Update()
 	{
-		for (var i = 0; i < m_LuaFunctionCallIterations; i++)
+		using (m_CallLuaMarker.Auto())
 		{
-			var func = m_LuaState.Environment[LuaFunctionName];
-			var luaFunction = func.Read<LuaFunction>();
-			m_LuaState.TopLevelAccess.RunAsync(luaFunction);
+			for (var i = 0; i < m_LuaFunctionCallIterations; i++)
+			{
+				m_LookupMarker.Begin();
+				var func = m_LuaState.Environment[CallLuaFunction];
+				var luaFunction = func.Read<LuaFunction>();
+				var thread = m_LuaState.TopLevelAccess;
+				m_LookupMarker.End();
+
+				thread.RunAsync(luaFunction);
+			}
+		}
+
+		using (m_CallCSharpMarker.Auto())
+		{
+			for (var i = 0; i < m_LuaFunctionCallIterations; i++)
+			{
+				m_LookupMarker.Begin();
+				var func = m_LuaState.Environment[CallCSharpMethod];
+				var luaFunction = func.Read<LuaFunction>();
+				var thread = m_LuaState.TopLevelAccess;
+				m_LookupMarker.End();
+
+				thread.RunAsync(luaFunction);
+			}
 		}
 	}
 
-	public void SetIterations(Slider slider)
+	public void SetIterations(Slider slider) => m_LuaFunctionCallIterations = (Int32)slider.value;
+}
+
+[LuaObject]
+public partial class UsedByLua
+{
+	[LuaMember]
+	public Boolean CSharpMethod()
 	{
-		m_LuaFunctionCallIterations = (int)slider.value;
+		Debug.Log("called");
+		return true;
 	}
 }
